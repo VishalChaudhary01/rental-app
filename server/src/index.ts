73c107c0951093b39bf8ab7a9690e578fd919242
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-03-09 23:04:09 +0530

    centralize error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 
 import { PORT } from './config/env';
+import { errorHandler, ErrorResponse } from './middlewares/errorHandler';
 
 const app = express();
 
@@ -16,6 +17,11 @@ app.get('/', (req, res) => {
   res.send('Healthy Server');
 });
 
+app.all('*', () => {
+  throw new ErrorResponse('Resource not found', 404);
+});
+
+app.use(errorHandler);
 app.listen(PORT, () =>
   console.log(`server started on http://localhost:${PORT}`)
 );
---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { NODE_ENV } from '../config/env';
+
+export class ErrorResponse extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  if (NODE_ENV === 'development') {
+    console.error('Error: ', err);
+  }
+
+  res.status(statusCode).json({ success: false, message });
+};
